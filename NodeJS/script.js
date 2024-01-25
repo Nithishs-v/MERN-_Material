a57e1fe5ccc842d5fc755bdd3f738da5c95f86f6
shri-2109 <20euec143@skcet.ac.in>
2024-01-25 15:45:54 +0530

    post request

--- NodeJS/script.js
+++ NodeJS/script.js
@@ -1,8 +1,30 @@
 const button = document.getElementById('button')
+const loginButton = document.getElementById('login-button')
+
 button.addEventListener('click', async function() {
-    // Sending a request from the client
-    const respObj = await fetch('/get-data')
-    // response object -> json data
-    const data = await respObj.json()
+    // Sending a request from the client - get request
+    // const respObj = await fetch('/get-data')
+    // // response object -> json data
+    // const data = await respObj.json()
+    // console.log(data)
+
+    // post request
+})
+
+loginButton.addEventListener('click', async function() {
+    // sending a post request
+    const respObj = await fetch('/verify-user', {
+        method : 'POST',
+        body : JSON.stringify({
+            "username" : "shri",
+            "password" : 1234
+        }),
+        headers : {
+            Accept : 'application/json',
+            'Content-Type' : 'application/json'
+        }
+    })
+    const data = await respObj
+    .json()
     console.log(data)
 })
--- NodeJS/server.cjs
+++ NodeJS/server.cjs
@@ -1,8 +1,11 @@
 // import in js -> require in cjs
 // export {} in js -> module.exports = {} in cjs
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
 
 // Capturing the request
 app.get('/get-data', function(request, response) {
@@ -14,6 +17,19 @@ app.get('/get-data', function(request, response) {
     }
     // sending the response
     response.status(200).json(respData)
+})
+
+app.post('/verify-user', function(request, response) {
+    console.log(request.body)
+    if(request.body.username === 'shri' && request.body.password === 1234) {
+        response.status(200).json({
+            "validation" : "user validated"
+        })
+    } else {
+        response.status(200).json({
+            "validation" : "invalid user"
+        })
+    }
 })
 
 app.listen(8000)
